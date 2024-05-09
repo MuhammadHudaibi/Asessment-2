93b9531953fe14ec210e3b241cc8c22051258099
Muhammad Hudaibi <muhhudaibi11@gmail.com>
2024-05-09 20:28:36 +0700

    done

--- app/src/main/java/com/d3if3010/asesmen2/MainActivity.kt
+++ app/src/main/java/com/d3if3010/asesmen2/MainActivity.kt
@@ -6,12 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.d3if3010.asesmen2.navigation.SetupNavGraph
-import com.d3if3010.asesmen2.ui.screen.MainScreen
 import com.d3if3010.asesmen2.ui.theme.Asesmen2Theme
 
 class MainActivity : ComponentActivity() {
--- app/src/main/java/com/d3if3010/asesmen2/ui/screen/MainScreen.kt
+++ app/src/main/java/com/d3if3010/asesmen2/ui/screen/MainScreen.kt
@@ -18,7 +18,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
